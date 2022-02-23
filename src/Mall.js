24d449f17abd7f4e3a535b8552cba5dd7c6894a9
jeonghwa,park <pjh445@gmail.com>
2022-02-23 22:16:46 +0900

    Add files via upload

---
+++ src/Mall.js
@@ -0,0 +1,5 @@
+function Mall() {
+  return <b>코엑스몰 점</b>;
+}
+
+export default Mall;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Mall from './Mall.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Chain(props) {
+  return <h2>나는 큰 맘을 먹고  {props.shop} <Mall />에서 구찌백을
+  질렀습니다.</h2>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Chain shop="스타필드"/>, document.getElementById('root'));
